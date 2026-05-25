c1a21a380197e2632712819f8f7149b5f89e04c7
amjid <ishaq.amjid@gmail.com>
2026-05-26 01:53:41 +0300

    Initial commit

--- app/src/main/java/com/isaac/souqalghiyar/presentation/request_parts/RequestPartsScreen.kt
+++ app/src/main/java/com/isaac/souqalghiyar/presentation/request_parts/RequestPartsScreen.kt
@@ -1,7 +1,6 @@
 package com.isaac.souqalghiyar.presentation.request_parts
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
