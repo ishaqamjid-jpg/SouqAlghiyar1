f7a940b6c3270880ed8a32199224960d0dcadae7
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-05-28 21:08:41 +0300

    Update AppModule.kt

--- app/src/main/java/com/isaac/souqalghiyar/di/AppModule.kt
+++ app/src/main/java/com/isaac/souqalghiyar/di/AppModule.kt
@@ -3,8 +3,10 @@ package com.isaac.souqalghiyar.di
 import android.content.Context
 import android.content.SharedPreferences
 import com.google.firebase.firestore.FirebaseFirestore
+import com.isaac.souqalghiyar.data.repository.AuthRepositoryImpl
 import com.isaac.souqalghiyar.data.repository.MainRepositoryImpl
 import com.isaac.souqalghiyar.data.repository.OrderRepositoryImpl
+import com.isaac.souqalghiyar.domain.repository.AuthRepository
 import com.isaac.souqalghiyar.domain.repository.MainRepository
 import com.isaac.souqalghiyar.domain.repository.OrderRepository
 import dagger.Module
@@ -18,32 +20,33 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    // توفير Firebase Firestore للاتصال بقاعدة البيانات
     @Provides
     @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore {
         return FirebaseFirestore.getInstance()
     }
 
-    // توفير SharedPreferences (ملف الجلسة تذكرني)
     @Provides
     @Singleton
     fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
         return context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
     }
 
-    // توفير MainRepository الخاص بالشاشة الرئيسية وعرض الإعلانات
+    @Provides
+    @Singleton
+    fun provideAuthRepository(db: FirebaseFirestore, sharedPreferences: SharedPreferences): AuthRepository {
+        return AuthRepositoryImpl(db, sharedPreferences)
+    }
+
     @Provides
     @Singleton
     fun provideMainRepository(firestore: FirebaseFirestore): MainRepository {
         return MainRepositoryImpl(firestore)
     }
 
-    // توفير OrderRepository الخاص برفع طلبات قطع الغيار الجديدة
     @Provides
     @Singleton
     fun provideOrderRepository(firestore: FirebaseFirestore): OrderRepository {
         return OrderRepositoryImpl(firestore)
     }
-
 }
