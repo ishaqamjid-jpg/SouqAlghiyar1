241ca60c5bfd91de4ba72a48d3f2cdf9c4b9ddd1
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-06-05 04:02:06 +0300

    Update OrderRepository.kt

--- app/src/main/java/com/isaac/souqalghiyar/domain/repository/OrderRepository.kt
+++ app/src/main/java/com/isaac/souqalghiyar/domain/repository/OrderRepository.kt
@@ -6,18 +6,10 @@ import com.isaac.souqalghiyar.domain.model.OrderWithItems
 import kotlinx.coroutines.flow.Flow
 
 interface OrderRepository {
-    // 1. تم التعديل هنا لتستقبل قائمة من القطع (List<OrderItem>) بصيغة الجمع
     suspend fun submitOrderWithItems(order: Order, items: List<OrderItem>): Result<Unit>
-
-    // 2. جلب أقسام القطع
     fun getCategories(): Flow<List<String>>
-
-    // 3. جلب أنواع الجودة
     fun getQualityTypes(): Flow<List<String>>
-
-    // 4. جلب طلبات المستخدم
+    fun getLocations(): Flow<List<String>>
     fun getUserOrders(userId: String): Flow<List<OrderWithItems>>
-
-    // 5. تحديث حالة الطلب
     suspend fun updateOrderStatus(orderId: String, newStatus: String): Result<Unit>
 }
