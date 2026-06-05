c31c68f091c4029e544cf816bad1197b3477d7cd
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-06-05 04:03:13 +0300

    Update MainRepository.kt

--- app/src/main/java/com/isaac/souqalghiyar/domain/repository/MainRepository.kt
+++ app/src/main/java/com/isaac/souqalghiyar/domain/repository/MainRepository.kt
@@ -1,10 +1,9 @@
 package com.isaac.souqalghiyar.domain.repository
 
-
-
 import com.isaac.souqalghiyar.domain.model.Advertisement
 import kotlinx.coroutines.flow.Flow
 
 interface MainRepository {
-    suspend fun getAdvertisements(): Flow<List<Advertisement>>
+    fun getActiveAdvertisements(): Flow<List<Advertisement>>
+    fun getBrands(): Flow<List<String>> // لجلب الماركات
 }
