5289e5fb64fa2344c486e253d75e1ab6eb9f7d4b
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-06-25 18:29:07 +0300

    Create users.kt

---
+++ app/src/main/java/com/isaac/souqalghiyar/domain/model/users.kt
@@ -0,0 +1,13 @@
+package com.isaac.souqalghiyar.domain.model
+
+import com.google.firebase.Timestamp
+
+data class users(
+    val user_id: String = "",
+    val phone_number: String = "",
+    val display_name: String = "",
+    val fcm_token: String = "",
+    val status: String = "active",
+     val number_of_rejections: 
+    val created_at: Timestamp? = null
+)
