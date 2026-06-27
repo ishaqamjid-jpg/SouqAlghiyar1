bbba36a3ee24df7fea03944afd42140299b3de1c
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-06-27 18:46:09 +0300

    Update MainViewModel.kt

--- app/src/main/java/com/isaac/souqalghiyar/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/isaac/souqalghiyar/presentation/main/MainViewModel.kt
@@ -18,7 +18,7 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: MainRepository,
-    private val userRepository: UserRepository // تمت الإضافة
+    private val userRepository: UserRepository
 ) : ViewModel() {
 
     private val _currentUser = MutableStateFlow<users?>(null)
