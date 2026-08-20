c45cb47d1af8162d6db65bbdd0a378225e1894b6
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-08-20 20:53:44 +0300

    Update NotificationRepository.kt

--- app/src/main/java/com/isaac/souqalghiyar/domain/repository/NotificationRepository.kt
+++ app/src/main/java/com/isaac/souqalghiyar/domain/repository/NotificationRepository.kt
@@ -1,10 +1,12 @@
 package com.isaac.souqalghiyar.domain.repository
 
+import com.isaac.souqalghiyar.domain.model.PublicAdvertisement
 import com.isaac.souqalghiyar.domain.model.user_alarm
 import kotlinx.coroutines.flow.Flow
 
 interface NotificationRepository {
-    fun getUserNotifications(userId: String): Flow<List<user_alarm>>
+    fun getUserAlarms(userId: String): Flow<List<user_alarm>>
+    fun getPublicAds(): Flow<List<PublicAdvertisement>>
     suspend fun markNotificationAsRead(alarmId: String): Result<Unit>
     suspend fun deleteNotification(alarmId: String): Result<Unit>
 }
